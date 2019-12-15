c60f9b8a5ab9face68dbb221d29c0b433a3d67e0
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2019-12-15 18:26:55 -0300

    delegate implementation

---
+++ DelegateProtocol/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  DelegateProtocol
+//
+//  Created by carloshenrique.carmo on 15/12/19.
+//  Copyright © 2019 carloshenrique.carmo. All rights reserved.
+//
+
+import UIKit
+
+struct Profile {
+    let icon: String
+    let accentColor: UIColor
+    let name: String
+    let info: String
+}
